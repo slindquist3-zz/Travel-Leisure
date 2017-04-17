284a60c0926cfb479c0cf24dd91c9f6cd00d35cb
slindquist3 <slindquist3@gmail.com>
2017-04-17 17:24:50 -0400

    create button

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import Button from './Button.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-
-          <h2>Welcome to React</h2>
-        <p>
-          To get started, edit and save to reload.
-        </p>
+        <Button />
       </div>
-
     );
   }
 }
