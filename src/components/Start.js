0153db04d3f42d65b9e7bb472e2d6c4b79173074
slindquist3 <slindquist3@gmail.com>
2017-04-21 13:19:13 -0400

    edit landing copy

--- src/components/Start.js
+++ src/components/Start.js
@@ -10,7 +10,7 @@ class Start extends Component {
     return (
       <div id="container">
         <h1>Travel / Leisure</h1>
-          <p>Plan your trip with just 4 buttons.</p>
+          <p>Plan your destination with just 4 buttons.</p>
         <button onClick={this.handleClick.bind(this)}>Plan Trip</button>
       </div>
     );
